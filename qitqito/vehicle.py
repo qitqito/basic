3eb24f3657b28d6174aba38d618354d964e2effa
qitqito <qitqitqito@gmail.com>
2020-07-09 21:24:12 +0900

    20200709Stage4

--- qitqito/vehicle.py
+++ qitqito/vehicle.py
@@ -1,6 +1,7 @@
 class Vehicle:
-    def __init__(self, velocity):
+    def __init__(self, velocity, name):
         self.velocity = velocity
+        self.name = name
 
     def __call__(self, velocity):
         self.velocity += velocity
@@ -11,31 +12,48 @@ class Vehicle:
     def __repr__(self):
         return 'Velocity is {}.'.format(self.velocity)
     
+    def __getitem__(self, key):
+        return self.name[key]
+
+    def __setattr__(self, name, value):
+        super().__setattr__(name, value) # これが無いとプロパティに代入されない
+        message = "{} is {}"
+        print(message.format(name, value))
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.velocity >= 110:
+            raise StopIteration()
+        self.velocity += 1
+        return self.velocity
+
     def __add__(self, other):
         if isinstance(other, Vehicle):
-            return Vehicle(self.velocity + other.velocity)
+            return Vehicle(self.velocity + other.velocity, self.name)
         elif isinstance(other, int):
-            return Vehicle(self.velocity + other)
+            return Vehicle(self.velocity + other, self.name)
         else:
             raise TypeError()
     
     def __radd__(self, other):
         if isinstance(other, int):
-            return Vehicle(other + self.velocity)
+            return Vehicle(other + self.velocity, self.name)
         else:
             raise TypeError()
     
     def __mul__(self, other):
         if isinstance(other, Vehicle):
-            return Vehicle(self.velocity * other.velocity)
+            return Vehicle(self.velocity * other.velocity, self.name)
         elif isinstance(other, int):
-            return Vehicle(self.velocity * other)
+            return Vehicle(self.velocity * other, self.name)
         else:
             raise TypeError()
     
     def __rmul__(self, other):
         if isinstance(other, int):
-            return Vehicle(other * self.velocity)
+            return Vehicle(other * self.velocity, self.name)
         else:
             raise TypeError()
     
